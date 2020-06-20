e01cbe49ef81a27cd73dc4594d099eb5eb7368f5
edwint88 <tuzaredwin@gmail.com>
2020-06-20 15:40:52 +0200

    adjust mat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 //material
-import {MatDividerModule} from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonModule } from '@angular/material/button';
-import { PokerTableComponent } from './poker-table/poker-table.component';
-import { PokerStatsComponent } from './poker-stats/poker-stats.component';
-import { PokerCardComponent } from './poker-card/poker-card.component';
+import {PokerTableComponent} from './poker-table/poker-table.component';
+import {PokerStatsComponent} from './poker-stats/poker-stats.component';
+import {PokerCardComponent} from './poker-card/poker-card.component';
 import {DragDropModule} from "@angular/cdk/drag-drop";
+import {DemoMaterialModule} from "./material-module";
 
 
 @NgModule({
@@ -25,10 +21,7 @@ import {DragDropModule} from "@angular/cdk/drag-drop";
         BrowserAnimationsModule,
         FormsModule,
         //material
-        MatDividerModule,
-        MatIconModule,
-        MatTooltipModule,
-        MatButtonModule,
+        DemoMaterialModule,
         DragDropModule
     ],
   declarations: [ AppComponent, FooterComponent, HeaderComponent, PokerTableComponent, PokerStatsComponent, PokerCardComponent ],
